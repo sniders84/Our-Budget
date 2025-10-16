b43ec744299288bc16cd8e7f6586bb826ef5ff94
sniders84 <sniders1984@gmail.com>
2025-10-16 06:16:04 -0400

    Create BillsTab.js

---
+++ components/BillsTab.js
@@ -0,0 +1,8 @@
+export default function BillsTab() {
+  return (
+    <div className="bg-white p-4 rounded shadow">
+      <h2 className="font-semibold mb-2">Bills</h2>
+      <p>Bill tracking calendar and table will go here (coming soon)</p>
+    </div>
+  )
+}
