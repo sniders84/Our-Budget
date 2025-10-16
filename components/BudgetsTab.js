0acb9940d39b0bcb0dcf2faefb92280ce85b5da1
sniders84 <sniders1984@gmail.com>
2025-10-16 06:15:48 -0400

    Create BudgetsTab.js

---
+++ components/BudgetsTab.js
@@ -0,0 +1,8 @@
+export default function BudgetsTab() {
+  return (
+    <div className="bg-white p-4 rounded shadow">
+      <h2 className="font-semibold mb-2">Budgets</h2>
+      <p>Budget cards and progress bars will go here (coming soon)</p>
+    </div>
+  )
+}
