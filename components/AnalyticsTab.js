e84283d18db8bf685bbe99837b99cc2091255fb7
sniders84 <sniders1984@gmail.com>
2025-10-16 06:16:20 -0400

    Create AnalyticsTab.js

---
+++ components/AnalyticsTab.js
@@ -0,0 +1,8 @@
+export default function AnalyticsTab() {
+  return (
+    <div className="bg-white p-4 rounded shadow">
+      <h2 className="font-semibold mb-2">Analytics</h2>
+      <p>Charts and insights will go here (coming soon)</p>
+    </div>
+  )
+}
