492d1fc805f745d0860ab26df0cc7c467a7b7a54
sniders84 <sniders1984@gmail.com>
2025-10-16 06:16:35 -0400

    Create AccountsTab.js

---
+++ components/AccountsTab.js
@@ -0,0 +1,8 @@
+export default function AccountsTab() {
+  return (
+    <div className="bg-white p-4 rounded shadow">
+      <h2 className="font-semibold mb-2">Accounts</h2>
+      <p>Linked accounts overview will go here (coming soon)</p>
+    </div>
+  )
+}
