5594234ace5eec44daaabe27eb567894502818a7
sniders84 <sniders1984@gmail.com>
2025-10-16 06:15:32 -0400

    Create TransactionsTab.js

---
+++ components/TransactionsTab.js
@@ -0,0 +1,8 @@
+export default function TransactionsTab() {
+  return (
+    <div className="bg-white p-4 rounded shadow">
+      <h2 className="font-semibold mb-2">Transactions</h2>
+      <p>Transaction table will go here (coming soon)</p>
+    </div>
+  )
+}
